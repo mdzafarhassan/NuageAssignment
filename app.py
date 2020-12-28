17855e08fe34d078e235a59ac1d1960b2967c0ea
Md Zafar Hassan <mdzafarhassan@yahoo.com>
2020-12-28 14:56:47 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from helper import ProductSearch
+import threading
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db=SQLAlchemy(app)
+
+
+class ProductMaster(db.Model):
+    id = db.Column(db.Integer, db.Sequence('seq_reg_id', start=1, increment=1), primary_key=True)
+    product_name = db.Column(db.String(200), nullable=False)
+    product_url = db.Column(db.String(500), nullable=False)
+    product_image = db.Column(db.String(500), nullable=False)
+    product_price = db.Column(db.Integer, nullable=False)
+    store = db.Column(db.String(25), nullable=False)
+    search_tag = db.Column(db.String(200), nullable=False)
+
+    def __rep__(self):
+        return '<Task %r>'  % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        keyword = request.form['search_keyword']
+        if not keyword or keyword=='':
+            return render_template('index.html')
+
+        print("keyword  ::  ",keyword)
+        p1 = threading.Thread(target=update_db_data, args=[keyword])
+        p1.start()
+        product_list = ProductMaster.query.filter(ProductMaster.product_name.ilike(f"%{keyword}%"), ProductMaster.search_tag.ilike(f"%{keyword}%"))[:50]
+        print(product_list)
+        return render_template('index.html', products=product_list, data='Search Result')
+    return render_template('index.html')
+
+
+def update_db_data(keyword):
+    products = ProductSearch()
+    product_list = products(keyword)
+    print(len(product_list))
+    for product in product_list:
+        try:
+            ProductMaster.query.filter_by(product_name=product['product_name'])[0]
+        except:
+            row = ProductMaster(product_name=product['product_name'], product_url=product['product_url'], product_image=product['product_image'], product_price=product['product_price'], store=product['store'], search_tag=keyword)
+            db.session.add(row)
+            db.session.commit()
+            pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ helper.py
@@ -0,0 +1,111 @@
+import time
+import requests
+import multiprocessing
+import threading
+
+
+class ProductSearch():
+
+    def __init__(self):
+        self.products_list = []
+        self.display_list = []
+        self.api_done = []
+
+    def __call__(self, keyword):
+        self.keyword = keyword
+
+        p1 = threading.Thread(target=self.paytm_data)
+        p2 = threading.Thread(target=self.tatacliq_data)
+        p3 = threading.Thread(target=self.shopclues_data)
+
+        p1.start()
+        p2.start()
+        p3.start()
+
+
+        while len(self.display_list)<50:
+            if len(self.products_list) > 0:
+                self.display_list.append(self.products_list[0])
+                self.products_list.pop(0)
+            elif len(self.api_done) >= 3:
+                break
+            else:
+                time.sleep(0.5)
+        return self.display_list
+
+
+    def paytm_data(self):
+        page = 1
+        error = 0
+        while len(self.display_list)<50 and not error:
+            args = f"https://search.paytm.com/v2/search?userQuery={self.keyword}&page_count={page}&items_per_page=20"
+            r = requests.get(args)
+            try:
+                data_dict = r.json().get('grid_layout')[:]
+                data_dict[0]
+            except Exception as ex:
+                error = 1
+                self.api_done.append('Paytm')
+                return
+            for i in data_dict:
+                temp = {
+                    'product_name': i.get('name'),
+                    'product_url': i.get('url'),
+                    'product_image': i.get('image_url'),
+                    'product_price': i.get('offer_price'),
+                    'store': 'Paytm'}
+                self.add_product(temp)
+
+            self.api_done.append('Paytm')
+            page+=1
+
+    def tatacliq_data(self):
+        page = 1
+        error = 0
+        while len(self.display_list)<50 and not error:
+            args = f"http://api.shopclues.com/api/v11/search?q={self.keyword}&z={page}&key=d12121c70dda5edfgd1df6633fdb36c0"
+            r = requests.get(args)
+            try:
+                data_dict = r.json().get('products')[:]
+                data_dict[0]
+            except Exception as ex:
+                error = 1
+                self.api_done.append('TataCliq')
+                return
+
+            for i in data_dict:
+                temp = {'product_name': i.get('product'),
+                        'product_url': i.get('product_url'),
+                        'product_image': i.get('image_url'),
+                        'product_price': i.get('price'),
+                        'store': 'TataCliq'}
+                self.add_product(temp)
+
+            self.api_done.append('TataCliq')
+            page+=1
+
+    def shopclues_data(self):
+        page = 1
+        error = 0
+        while len(self.display_list)<50 and not error:
+            args = f"https://www.tatacliq.com/marketplacewebservices/v2/mpl/products/serpsearch?type=category&channel=mobile&pageSize=20&typeID=al&page={page}0&searchText={self.keyword}&isFilter=false&isTextSearch=true"
+            r = requests.get(args)
+            try:
+                data_dict = r.json().get('searchresult')[:]
+                data_dict[0]
+            except Exception as ex:
+                error = 1
+                self.api_done.append('ShopClue')
+                return
+            for i in data_dict:
+                temp = {'product_name': i.get('productname'),
+                        'product_url': i.get('url'),
+                        'product_image': i.get('imageURL'),
+                        'product_price': i.get('mrpPrice'),
+                        'store': 'ShopClue'}
+                self.add_product(temp)
+            self.api_done.append('ShopClue')
+            page+=1
+
+    def add_product(self, product):
+        self.products_list.append(product)
